7b45021d458e668a13a1aa8db86844277749adfe
Carl J <carlj1881@yahoo.com>
2020-09-12 01:45:49 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -5,8 +5,10 @@ def countdown(number)
   until number == 0
     number -= 1
     puts "#{number} SECOND(S)!"
+  end
+  return "HAPPY NEW YEAR!"
 end
-  puts "HAPPY NEW YEAR!"
+  
 
 
 def countdown_with_sleep(number)
